75bba52a977b5526dfc15993234a4c7a223b08a9
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2023-10-10 23:23:52 -0300

    Created classe Category

---
+++ src/main/java/br/com/rafaecommerce/entities/Category.java
@@ -0,0 +1,45 @@
+package br.com.rafaecommerce.entities;
+
+import java.util.Objects;
+
+public class Category {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
