ba20f1b413ef2c8f88fb97edd6f77c359021c023
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2023-10-10 22:56:19 -0300

    Projected Created

---
+++ src/main/java/br/com/rafaecommerce/RafaecommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafaecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafaecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafaecommerce/RafaecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RafaecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
