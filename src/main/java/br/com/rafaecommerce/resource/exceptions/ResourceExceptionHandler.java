ba28e2f7a6b4f8fa2b77e3bf2de0af3cfda8fe30
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2023-10-14 00:31:52 -0300

    Busca Por Id/Tratamento de Exceçoes

---
+++ src/main/java/br/com/rafaecommerce/resource/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package br.com.rafaecommerce.resource.exceptions;
+
+import br.com.rafaecommerce.services.exceptions.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityotFound(EntityNotFoundException e, HttpServletRequest request) {
+        StandardError error = new StandardError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setError("Entity not found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
--- src/main/java/br/com/rafaecommerce/services/CategoryService.java
+++ src/main/java/br/com/rafaecommerce/services/CategoryService.java
@@ -3,11 +3,13 @@ package br.com.rafaecommerce.services;
 import br.com.rafaecommerce.dto.CategoryDTO;
 import br.com.rafaecommerce.entities.Category;
 import br.com.rafaecommerce.repositories.CategoryRepository;
+import br.com.rafaecommerce.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,10 +17,19 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
         List<Category> list = categoryRepository.findAll();
         List<CategoryDTO> lisDTO = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
         return lisDTO;
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.orElseThrow(()->new EntityNotFoundException("Entity Not Found"));
+        CategoryDTO dto = new CategoryDTO(entity);
+        return dto;
     }
 }
